c9598fcaef28e12c60af30e1822bc42c1986ac8c
Abhay Kumar Mishra <abhaykumarmishra0369@gmail.com>
2025-11-16 11:33:52 +0530

    Update classifier.py

--- app/json_ingestion/classifier.py
+++ app/json_ingestion/classifier.py
@@ -9,25 +9,23 @@ def is_sql_like(data: Any) -> bool:
       - values are primitives (str, int, float, bool, None)
     """
 
-    # Must be a list
+
     if not isinstance(data, list):
         return False
 
     if len(data) == 0:
         return False
 
-    # Must be list of dicts
+
     if not all(isinstance(item, dict) for item in data):
         return False
 
-    # Keys must match (consistent schema)
     base_keys = set(data[0].keys())
 
     for item in data:
         if set(item.keys()) != base_keys:
             return False
 
-        # No nested dicts/lists allowed inside values
         for v in item.values():
             if isinstance(v, (dict, list)):
                 return False
