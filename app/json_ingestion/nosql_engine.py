1b3ef8e8ad17d7f373bbff139c09d5e0aa9995dc
Abhay Kumar Mishra <abhaykumarmishra0369@gmail.com>
2025-11-16 11:35:45 +0530

    Update nosql_engine.py

--- app/json_ingestion/nosql_engine.py
+++ app/json_ingestion/nosql_engine.py
@@ -11,11 +11,9 @@ def store_nosql_dataset(mongo_uri: str, db_name: str, collection_name: str, data
     db = client[db_name]
     collection = db[collection_name]
 
-    # List inserts (e.g., [{"a":1},{"b":2}])
     if isinstance(data, list):
         result = collection.insert_many(data)
         return len(result.inserted_ids)
 
-    # Single document (e.g., {"a":1})
     result = collection.insert_one(data)
     return 1
